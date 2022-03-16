9d874d49901d9a572440c4ac6ce0deffc47cc53d
HeikkiLu <heikki.luukkonen@gmail.com>
2022-03-16 11:30:16 +0200

    3.6 done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
@@ -51,9 +52,51 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'Name or number missing'
+        })
+    }
+
+    if(persons.some(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
 app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p>\n<p>${new Date()}</p>`)
 })
+
+const generateId = () => {
+    return Math.floor(Math.random()*1000)
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
